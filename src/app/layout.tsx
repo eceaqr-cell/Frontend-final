955ffca843c7424f29844ae3bd08012160822057
chhannon <eceaqr@gmail.com>
2026-09-22 17:56:47 +0700

    newuser

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,10 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Navbar from "@/components/shadcn-space/blocks/navbar-01/navbar";
+import Footer from "@/components/shadcn-space/blocks/footer-01/footer";
+import AgencyHeroSection from "@/components/shadcn-space/blocks/hero-01";
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,7 +27,11 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        <AgencyHeroSection/>
+        {children}
+         <Footer />
+        </body>
     </html>
   );
 }
